7b04e94909a6502c1a33a1f70990ca213168134f
Nitish Roy <nitishr833@gmail.com>
2022-08-24 23:49:12 +0530

    inital project commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "antd/dist/antd.css";
+import DemoScreen from "./demos/demos";
+function App() {
+  return (
+    <>
+      <DemoScreen />
+    </>
+  );
+}
+
+export default App;
---
+++ src/demos/demos.js
@@ -0,0 +1,18 @@
+import { Row, Col } from "antd";
+
+function DemoScreen() {
+  return (
+    <>
+      <Row
+        align="middle"
+        justify="center"
+        style={{ width: "100%", height: "100%", backgroundColor: "gray" }}
+      >
+        <Col >
+          <h1>Demo Screen</h1>
+        </Col>
+      </Row>
+    </>
+  );
+}
+export default DemoScreen;
