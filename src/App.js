064ebf59027ce98213bbbbbfcc2e6a0fca8a6444
Nitish Roy <nitishr833@gmail.com>
2022-08-25 01:06:52 +0530

    task bar with logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import "antd/dist/antd.css";
 import DemoScreen from "./demos/demos";
+import TaskBar from "./layout/taskbar";
 function App() {
   return (
     <>
-      <DemoScreen />
+      {/* <DemoScreen /> */}
+      <TaskBar />
     </>
   );
 }
---
+++ src/layout/taskbar.js
@@ -0,0 +1,63 @@
+import { Row, Image } from "antd";
+import "./taskbarstyle.css";
+import excellogo from "../assets/Taskbar/excel-logo.png";
+import folder from "../assets/Taskbar/folder.png";
+import MicrosoftEdge from "../assets/Taskbar/Microsoft-Edge.png";
+// import Notepad from "../assets/Taskbar/Notepad.png";
+import windows11 from "../assets/Taskbar/windows11.png";
+import Word from "../assets/Taskbar/Word.png";
+function TaskBar() {
+  return (
+    <>
+      <Row
+        align="middle"
+        justify="center"
+        style={{ width: "100%", height: "100%", backgroundColor: "gray" }}
+      >
+        <Row align="middle" justify="center" className="task-bar">
+          <div className="taskbar-icon">
+            <Image
+              width={35}
+              preview={false}
+              className="profile-image"
+              src={windows11}
+            />
+          </div>
+          <div className="taskbar-icon">
+            <Image
+              width={35}
+              preview={false}
+              className="profile-image"
+              src={Word}
+            />
+          </div>
+          <div className="taskbar-icon">
+            <Image
+              width={35}
+              preview={false}
+              className="profile-image"
+              src={MicrosoftEdge}
+            />
+          </div>
+          <div className="taskbar-icon">
+            <Image
+              width={35}
+              preview={false}
+              className="profile-image"
+              src={folder}
+            />
+          </div>
+          <div className="taskbar-icon">
+            <Image
+              width={35}
+              preview={false}
+              className="profile-image"
+              src={excellogo}
+            />
+          </div>
+        </Row>
+      </Row>
+    </>
+  );
+}
+export default TaskBar;
